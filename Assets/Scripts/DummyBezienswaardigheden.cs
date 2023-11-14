b5631ae9c4f2ef324d634a9271788c91a9a8213e
ThomasVNL98 <tom@vonkplaza.net>
2023-11-14 22:34:51 +0100

    List and functionality added
    Added a scrollable list
    Added dummydata which is displayed in the list

---
+++ Assets/Scripts/DummyBezienswaardigheden.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class DummyBezienswaardigheden
+{
+    public string name;
+    public string description;
+    public int locationX;
+    public int locationY;
+
+}
